76e8905fd7e261a3dae7d37901db42c0893dbdd1
Vincenzo Sicurella <sicurella12@gmail.com>
2020-11-11 22:58:29 -0500

    Pull README html from repo

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const sitemap = document.getElementById('sitemap-section')
 const sidebar = document.getElementById('product-sidebar-section')
 const contentBody = document.getElementById('site-body-section')
 
-window.onresize = () => {
-    
-}
+let readme;
+let readmeVersion;
+let releases;
 
 function addDownloadSection(parent, productCode, name, release) {
     let header = document.createElement('h2')
@@ -21,8 +21,6 @@ function addDownloadSection(parent, productCode, name, release) {
 }
 
 function addToDownloadSection(parent, platform, url) {
-//    <a href="#" class="download btn"><img src="./assets/win-logo.svg"></a>
-
     let imgSrc = './assets/' + platform + '-logo.'
 
     if (platform === 'linux' || platform === 'ios') imgSrc += 'png'
@@ -39,43 +37,70 @@ function addToDownloadSection(parent, platform, url) {
     parent.appendChild(download)
 }
 
-fetch('https://api.github.com/repos/vsicurella/SuperVirtualKeyboard/releases', {
+// Pull README.md html
+fetch('http://api.github.com/repos/vsicurella/SuperVirtualKeyboard/readme', {
     method: 'GET',
     headers: {
-        'Accept': 'application/vnd.github.v3+json'
+        'Accept': 'application/vnd.github.v3.html'
     }
 })
-.then(response => response.json())
-.then(response => {
-    latest = response[0]
-    let productCode = 'svk'
-    let container = document.getElementById(productCode + '-sidebar')
-    let flavors = {}
-    latest.assets.forEach(a =>{
+.then(response => response.text())
+.then(result => {
+    readme = document.createRange().createContextualFragment(result)
+    readmeVersion = result.match(/v\d\.\d\d/)[0].slice(1)
 
-        console.log("Reading asset:")
-        console.log(a)
-        
-        let type = a.name.match(/(_VSTi)|((_Android)|(_iOS))/)
-        if (type)
-            type = type[0] === '_VSTi' ? 'VSTi' : 'Mobile'
-        else
-            type = "Standard"
+    let latestVersion = document.createElement('h2')
+    latestVersion.innerText = "Master branch version: " + readmeVersion
+    document.getElementById('svk-sidebar').appendChild(latestVersion)
 
-        let section = flavors['type']
+    // remove some nodes
+    Array.from(readme.querySelectorAll('path')).forEach(el => el.remove())
+    let ps = Array.from(readme.querySelectorAll('p'))
+    ps[0].remove()
+    ps[1].querySelector('img').src = './assets/svk-hero0.gif'
 
-        if (!section)
-        {
-            section =  addDownloadSection(container, productCode, type, latest.tag_name)
-            flavors['type'] = section
-            console.log("Added " + type + " section")
-            console.log(section)
-        }
-
-        let platform = a.name.match(/(_Win)|(_Mac)|(_Linux)|(_Android)|(_iOS)/)[0].slice(1).toLowerCase()
-        console.log("Platform: " + platform)
-
-        addToDownloadSection(section, platform, a.browser_download_url)
-    })
+    document.getElementById('site-body-container').appendChild(readme)
 })
+.then(
+    // Find latest release and build download buttons
+    fetch('https://api.github.com/repos/vsicurella/SuperVirtualKeyboard/releases', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/vnd.github.v3+json'
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        releases = response
+        latest = releases[0]
+        let productCode = 'svk'
+        let container = document.getElementById(productCode + '-sidebar')
+        let flavors = {}
+        latest.assets.forEach(a =>{
 
+            console.log("Reading asset:")
+            console.log(a)
+            
+            let type = a.name.match(/(_VSTi)|((_Android)|(_iOS))/)
+            if (type)
+                type = type[0] === '_VSTi' ? 'VSTi' : 'Mobile'
+            else
+                type = "Standard"
+
+            let section = flavors['type']
+
+            if (!section)
+            {
+                section =  addDownloadSection(container, productCode, type, latest.tag_name)
+                flavors['type'] = section
+                console.log("Added " + type + " section")
+                console.log(section)
+            }
+
+            let platform = a.name.match(/(_Win)|(_Mac)|(_Linux)|(_Android)|(_iOS)/)[0].slice(1).toLowerCase()
+            console.log("Platform: " + platform)
+
+            addToDownloadSection(section, platform, a.browser_download_url)
+        })
+    })
+)
