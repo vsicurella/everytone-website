8e17b2cce5e75279b635ccbde7a8ae4a9da7e9ff
Vincenzo Sicurella <sicurella12@gmail.com>
2020-11-12 22:31:04 -0500

    Reformatting, abstractions to product content

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const sitemap = document.getElementById('sitemap-section')
-const sidebar = document.getElementById('product-sidebar-section')
-const contentBody = document.getElementById('site-body-section')
+const sidebarContainer = document.getElementById('product-sidebar-container')
+const contentContainer = document.getElementById('site-body-container')
 
-let readme;
-let readmeVersion;
-let releases;
+let currentBody;
+let currentSidebar;
+let currentGitUrl;
 
-function addDownloadSection(parent, productCode, name, release) {
+function addDownloadSection(parent, productCode, name, version) {
     let header = document.createElement('h2')
-    header.innerText = name + ': ' + release
+    header.innerText = name + ': v' + version
 
     let section = document.createElement('div')
     section.id = productCode + '-' + name.toLowerCase() + '-download-container'
@@ -37,50 +36,67 @@ function addToDownloadSection(parent, platform, url) {
     parent.appendChild(download)
 }
 
-// Pull README.md html
-fetch('http://api.github.com/repos/vsicurella/SuperVirtualKeyboard/readme', {
-    method: 'GET',
-    headers: {
-        'Accept': 'application/vnd.github.v3.html'
-    }
-})
-.then(response => response.text())
-.then(result => {
-    readme = document.createRange().createContextualFragment(result)
-    readmeVersion = result.match(/v\d\.\d\d/)[0].slice(1)
+async function setupProductBody(owner, repoName, productCode) {
 
-    let latestVersion = document.createElement('h2')
-    latestVersion.innerText = "Master branch version: " + readmeVersion
-    document.getElementById('svk-sidebar').appendChild(latestVersion)
+    currentGitUrl = 'http://api.github.com/repos/' + [owner, repoName, ''].join('/')
+    
+    containerId = productCode + '-body'
+    currentBody = document.getElementById('product-body-overview').cloneNode()
+    currentBody.id = containerId
+    currentBody.classList.toggle('template')
+    currentBody.classList.toggle('active')
 
-    // remove some nodes
-    Array.from(readme.querySelectorAll('path')).forEach(el => el.remove())
-    let ps = Array.from(readme.querySelectorAll('p'))
-    ps[0].remove()
-    ps[1].querySelector('img').src = './assets/svk-hero0.gif'
+    sidebarId = productCode + '-sidebar'
+    currentSidebar = document.getElementById('product-sidebar').cloneNode(true)
+    currentSidebar.id = sidebarId
+    currentSidebar.classList.toggle('template')
+    currentSidebar.classList.toggle('active')
 
-    document.getElementById('site-body-container').appendChild(readme)
-})
-.then(
-    // Find latest release and build download buttons
-    fetch('https://api.github.com/repos/vsicurella/SuperVirtualKeyboard/releases', {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/vnd.github.v3+json'
-        }
-    })
-    .then(response => response.json())
-    .then(response => {
-        releases = response
-        latest = releases[0]
-        let productCode = 'svk'
-        let container = document.getElementById(productCode + '-sidebar')
+    contentContainer.append(currentBody)
+    sidebarContainer.appendChild(currentSidebar)
+
+    // Pull README.md html
+    let readme = await fetch(
+        currentGitUrl + 'readme', {
+            method: 'GET',
+            headers: { 'Accept': 'application/vnd.github.v3.html' }
+        })
+    if (!readme.ok) {
+        readme = '<span> 404 </span>'
+    } else {
+        readme = await readme.text()
+        
+        let mainBranchVersion = readme.match(/v\d\.\d\d/)[0].slice(1)
+
+        // Pull latest source version
+        let latestVersion = currentSidebar.querySelector('h2')
+        latestVersion.innerText += 'v' + mainBranchVersion
+        currentSidebar.querySelector('#github-link').href += [owner, repoName, ''].join('/')
+
+        readme = document.createRange().createContextualFragment(readme)
+
+        // remove some nodes
+        Array.from(readme.querySelectorAll('path')).forEach(el => el.remove())
+        let ps = Array.from(readme.querySelectorAll('p'))
+        ps[0].remove()
+        ps[1].querySelector('img').src = './assets/svk-hero0.gif'
+
+        currentBody.appendChild(readme)
+
+        // Find latest release and build download buttons
+        let releases = await fetch(
+            currentGitUrl + 'releases', {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/vnd.github.v3+json'
+                }
+            })
+            .then(response => response.json())
+
+        let latest = releases[0]
         let flavors = {}
-        latest.assets.forEach(a =>{
+        latest.assets.forEach(a => {
 
-            console.log("Reading asset:")
-            console.log(a)
-            
             let type = a.name.match(/(_VSTi)|((_Android)|(_iOS))/)
             if (type)
                 type = type[0] === '_VSTi' ? 'VSTi' : 'Mobile'
@@ -89,18 +105,16 @@ fetch('http://api.github.com/repos/vsicurella/SuperVirtualKeyboard/readme', {
 
             let section = flavors['type']
 
-            if (!section)
-            {
-                section =  addDownloadSection(container, productCode, type, latest.tag_name)
+            if (!section) {
+                section =  addDownloadSection(currentSidebar, productCode, type, latest.tag_name)
                 flavors['type'] = section
-                console.log("Added " + type + " section")
-                console.log(section)
             }
 
             let platform = a.name.match(/(_Win)|(_Mac)|(_Linux)|(_Android)|(_iOS)/)[0].slice(1).toLowerCase()
-            console.log("Platform: " + platform)
 
             addToDownloadSection(section, platform, a.browser_download_url)
         })
-    })
-)
+    }
+}
+
+setupProductBody('vsicurella', 'SuperVirtualKeyboard', 'svk')
